b44051c015c4fb497410b3592ee28ad3bef1c69e
Divya <86043076+Divya4879@users.noreply.github.com>
2024-06-02 18:20:01 +0530

    Update Day6.py

---
+++ Day6/Day6.py
@@ -0,0 +1,21 @@
+# Day 6 Challenge
+
+print("A simple Secure Login Program")
+print()
+
+username = input("What is your first name? ")
+password = input("Enter your password: ")
+
+print()
+
+if username == "Divya" and password == "divs":
+  print("Welcome back Divya!👩🏻‍💻")
+
+elif username == "Riya" and password == "riyaDev4*":
+  print("Hello Riya! Glad to meet you again!😊")
+
+elif username == "Python" and password == "🐍shhh":
+  print("Oh my! Pythons can type nowadays!😬")
+
+else:
+  print("You are denied access here 👋")
