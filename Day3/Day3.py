d2975bfe662973d4c7a9c4fb6f4b3e37bc615377
Divya <86043076+Divya4879@users.noreply.github.com>
2024-06-02 17:30:48 +0530

    Update Day3.py

---
+++ Day3/Day3.py
@@ -0,0 +1,10 @@
+# Day 3 Challenge
+
+foodItem = input("Name an easy to prepare food item > ")
+plant = input("Name any plant > ")
+cookingMethod = input("Name a method of cooking > ")
+describingWord = input("What words describes your favourite dish? > ")
+diyItem = input("Name a DIY item > ")
+
+print("MENU")
+print(cookingMethod , foodItem , "with" , describingWord, plant , "on a bed of" , diyItem)
