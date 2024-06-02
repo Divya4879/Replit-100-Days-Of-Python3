8049a52b221f5caeb34e0fa22f91ee5d7b473693
Divya <86043076+Divya4879@users.noreply.github.com>
2024-06-02 17:26:58 +0530

    Update Day1.py

---
+++ Day1/Day1.py
@@ -0,0 +1,11 @@
+print("Divya")
+print("11th April, 2024")
+print("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day at the very least.
+I'll be using Replit, an amazing online IDE, so that I can share my work online easily!
+Happy coding 😊
+""")
+
+print("I am feeling 👩‍💻")
+print("You can follow my progress at replit.com/@divyasingh0803")
