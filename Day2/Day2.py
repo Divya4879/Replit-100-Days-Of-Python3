e198629608c84644b1664ab3413d5adf5c67efcc
Divya <86043076+Divya4879@users.noreply.github.com>
2024-06-02 17:28:55 +0530

    Update Day2.py

---
+++ Day2/Day2.py
@@ -0,0 +1,11 @@
+print("Let me ask you a few basic questions to get to know you better!")
+print()
+
+yourName = input("Your name?: ")
+favFood = input("Your go-to meal when you're sad?: ")
+favArtist = input("Your all time favourite artist?: ")
+print("Ooh! great artist!")
+yourCountry = input("In which country were you raised up?: ")
+
+print("Great getting to know you" + yourName)
+print("You seem dedicated towards improving your Python level.Congratulations for completing 2% of the challenge. Remember, don't give up!!")
