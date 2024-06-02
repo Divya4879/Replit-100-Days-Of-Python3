37dba7f42b3ee69875bf3d6d26c6ca2cff1297de
Divya <86043076+Divya4879@users.noreply.github.com>
2024-06-02 17:33:17 +0530

    Update Day4.py

---
+++ Day4/Day4.py
@@ -0,0 +1,43 @@
+yourName = input("Please enter your name: ",)
+print("\033[43m","\033[35m","Hey", yourName, "Welcome to my Adventure Stimulator. I am going to ask you a plethora of questions to help me create an epic story with you my friend, as its protagonist🤩!! The theme of this story is going to be Self-Growth and achieving our goals." ,"\033[0m","\033[0m")
+
+print()
+
+yourFavPerson = input("What's the name of your favorite person? ")
+print()
+yourSuperPower = input("What's your superpower? ")
+print()
+yourResidence = input("Where do you live? ")
+print()
+yourFavFood = input("Which food is your prized meal? ")
+print()
+
+
+print("\033[40m","\033[35m","Hello", yourName, "Your special ability of" , yourSuperPower, "can make sure that you become an important part of", yourFavPerson, "life as well", "☺️", "! Meanwhile, have some of that delicious", yourFavFood, "as you strategize how to do it, all the while getting better!","\033[0m","\033[0m")
+print()
+
+yourHabit = input("Which is the one thing you want to get better at, and you know that you need to? ")
+print()
+
+physicalHealth = input("What's the physical activity that you enjoy a lot, (it can be any physical activity, not just sports or workouts)? ")
+print()
+
+socialMedia = input("How much time do you spend on Social Media?(type in mins) ")
+print()
+
+print("\033[34m","Now, do some self-introspection and answer yourself, is it really worth it?" ,"\033[0m")
+print()
+
+stressLevel = input("On a scale of 0 to 10, rate your stress level on a typical day? ")
+print()
+
+print("\033[34m","You want to lower your stress, right?" ,"\033[0m")
+print()
+
+print("\033[40m","\033[35m","So, now's the main part. You want to get better at", yourHabit, "and have been for a while, right? Oh well, here's the hack, there's none! Go out there, and keep at it, you need to be consistent every day, for a minimum of 30 mins a day, for about a  month to develop this habit, and depending on circumstances, more time to get better at it.Meanwhile, do", physicalHealth, "for atleast 150 mins a week for basic health and if you want to improve your current physical health, do it for a max amount of 300 mins a day. Also, you want to reduce the", socialMedia ,"mins you spend on social media right, well, the most effective way to break a habit is to replace it with another habit. You can develop the habit of journaling, meditation, walking, doing", physicalHealth, "whilst drastically reducing the time spent on just scrolling the social media. Also, journaling, long walks, physical activities, meditation also help reduce our stress levels.","\033[0m","\033[0m")
+print()
+
+print("\033[40m","\033[35m","So, after a month, there has been a profound change in you. You have lowered your stress levels, spend a minimum amount of time on useless scrolling on social media, you're enjoying", physicalHealth, "while improving your physical & mental health. Last but not the least, you have made significant progress in", yourHabit, "and not only you are getting better at every aspect of your life, you've more confidence in yourself coz you know you can achieve whatever you want if you want it badly enough and work hard for it.","\033[0m","\033[0m")
+print()
+
+print("\033[40m","\033[35m","Also, do you still remember", yourFavPerson, "? Well, they're just waiting for you with a smile on their face, a sparkle in their eyes radiating warmth, pride, love, a tint of blush on their cheeks, a bouquet of your favourite flowers and their companionship!","\033[0m","\033[0m")
